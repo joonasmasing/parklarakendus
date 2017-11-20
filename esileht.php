5d06cebcb9cd5a039f41ab1b7f204817791f56b4
Tanel Kuklane <t1a2n3@users.noreply.github.com>
2017-11-20 14:15:36 +0200

    esileht

---
+++ esileht.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Parklarakendus</title>
+
+</head>
+
+<body bgcolor="#ADAD85">
+<center><h1>Tere tulemast!</h1>
+ <form action="/register.php"> 
+  <input type="submit" value="Registeeru">
+  <form action="/login.php"> 
+  <input type="submit" value="Logi sisse">
+  </form>
+<p></p>
+<img src="pildid/parkla.jpg" alt="parkla märk">
+</center>
+</body>
