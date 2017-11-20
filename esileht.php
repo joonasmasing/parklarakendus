316f481a6e32ea690a5b9805fd3cdc5b34f50bef
Tanel Kuklane <t1a2n3@users.noreply.github.com>
2017-11-20 15:39:32 +0200

    yoyo

--- esileht.php
+++ esileht.php
@@ -1,12 +1,17 @@
+<!DOCTYPE html>
+
 <?php
 
+$pildi_number=rand(1, 6);
 
 ?>
-<!DOCTYPE html>
+
 <html>
+
 <head>
-	<meta charset="utf-8">
+
 <title>Parklarakendus</title>
+
 </head>
 
 <body bgcolor="#ADAD85">
@@ -16,12 +21,28 @@
   <form action="/login.php"> 
   <input type="submit" value="Logi sisse">
   </form>
-	<p></p>
-<img src="pildid/parkla.jpg" alt="parkla märk">
+<p></p>
+
+<?php
+
+if ($pildi_number==1)
+{
+echo '<img src="pildid/parkla.jpg" alt="parkla märk">';
+}else if ($pildi_number==2)
+{
+echo '<img src="pildid/luksparkla.jpg" alt="luks parkla">';
+}else if ($pildi_number==3)
+{
+echo '<img src="pildid/muruparkla.jpg" alt="luks parkla">';
+}
+
+?>
+
+<img src="pildid/parkla.jpg" alt="parkla märk" style="width:500px;height:600px;">
+<img src="pildid/luksparkla.jpg" alt="luks parkla" style="width:500px;height:600px;">
+<img src="pildid/muruparkla.jpg" alt="muru parkla" style="width:500px;height:600px;">
+<img src="pildid/ostukesksueparkla.jpg" alt="ostukeskuse parkla" style="width:500px;height:600px;">
+<img src="pildid/siseparkla.jpg" alt="sise parkla" style="width:500px;height:600px;">
+<img src="pildid/tyhiparkla.jpg" alt="tyhi parkla" style="width:500px;height:600px;">
 </center>
-
-
 </body>
-
-
-
